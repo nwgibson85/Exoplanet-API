7c1261ac2a4ab032067fae45539f1c2f6234f53b
Nicholas Gibson <Nwgibson85@gmail.com>
2019-07-18 14:10:44 -0500

    functionality complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const apiKey = 'Bsa2YtF9x3coxqZqsYgqI4iAzpAsMgfhoRdKh0w4'; 
 const baseURL = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI';
 let x = 0;
+let time = '';
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -11,22 +12,19 @@ function formatQueryParams(params) {
 }
 
 function increaseX() {
-    return x ++,
+    x++;
     console.log(x);
 }
 
 function resetX() {
-    return x === 0,
+    x = 0;
     console.log(x);
 }
 
 function printRandomExoPlanets(displayedExoPs, Json) {
     $('#results-list').empty();
-    $('#compareSizeBox').empty();
     resetX();
-    console.log(Json[displayedExoPs[0]]);
     for (let i = 0; i < displayedExoPs.length; i++){
-      
         $('#results-list').append(
             `<h3>${Json[displayedExoPs[i]].pl_name}</h3>
             <label for="moreExoPlanetInfo">Choose what information you would like to display.</label> 
@@ -34,12 +32,12 @@ function printRandomExoPlanets(displayedExoPs, Json) {
                 <li class="li-hostStar">
                     <h3>${Json[displayedExoPs[i]].pl_name}'s host star info</h3>
                     <button class="starInfo" id='${x}SeeStarInfo' type='click'>Go!</button>
-                    <div class='${x}SeeStarInfo' id='${x}HostStar'>The host star ${Json[displayedExoPs[i]].pl_hostname} is ${Json[displayedExoPs[i]].st_dist} light years away from our Sun.</div>
+                    <div class='${x}SeeStarInfo' id='${x}HostStar'></div>
                 </li>
                 <li class='li-orbit'>
                     <h3>${Json[displayedExoPs[i]].pl_name}'s orbital period</h3>
                     <button class='orbit' id='${x}SeeOrbit' type='click'>Go!</button>
-                    <div class='${x}SeeOrbit' id='${x}Orbit'>It takes ${Json[displayedExoPs[i]].pl_name} ${Json[displayedExoPs[i]].pl_orbper} days to orbit ${Json[displayedExoPs[i]].pl_hostname}</div>
+                    <div class='${x}SeeOrbit' id='${x}Orbit'></div>
                 </li>
                 <li class='li-sizeCompare'>
                     <h3>See how big this ${Json[displayedExoPs[i]].pl_name} is compared to earth</h3>
@@ -47,7 +45,7 @@ function printRandomExoPlanets(displayedExoPs, Json) {
                     <div id='${x}SizeCompareBox'></div>
                 </li>
             </ul>
-            <a href='${Json[displayedExoPs[i]].pl_pelink}'>link to this planet's page in the Exoplanet Encyclopedia</a><span>  At the bottom of the page is a list of publications related to this Exoplanet</span>`
+            <a href='${Json[displayedExoPs[i]].pl_pelink}'>link to this planet's page in the Exoplanet Encyclopedia</a><span>  Scroll to the bottom of the page to see a list of publications related to this Exoplanet</span>`
         )
         increaseX();
     };
@@ -58,27 +56,75 @@ function printRandomExoPlanets(displayedExoPs, Json) {
 }
 
 function hideYoDivs(displayedExoPs) {
-    let zeta = 0;
-    
     for (let i = 0; i < displayedExoPs.length; i++) { 
-        console.log(zeta);
-        let hostStar = "#" + zeta + "HostStar";
-        let orbit = "#" + zeta + "Orbit";
-        let size = "#" + zeta + "SizeCompareBox";
+        let eta = 0;
+        console.log(eta);
+        let hostStar = "#" + eta + "HostStar";
+        let orbit = "#" + eta + "Orbit";
+        let size = "#" + eta + "SizeCompareBox";
         $(hostStar).hide();
         $(orbit).hide();
         $(size).hide();
-        zeta ++;  
+        eta++;  
     }
     console.log("divs hidden");
 }
 
+function printStarInfo(Json, displayedExoPs, delta) {
+    let epsilon = delta.charAt(1);
+    let divDesired = "#" + epsilon + "HostStar";
+    console.log(Json[displayedExoPs[epsilon]].st_dist);
+    $(divDesired).empty();
+    if (Json[displayedExoPs[epsilon]].st_dist === null) {
+        $(divDesired).append(
+            `The host star ${Json[displayedExoPs[epsilon]].pl_hostname}'s distance from our solar system has not been confirmed yet.`
+        )
+    }
+    else {
+        $(divDesired).append(
+            `The host star ${Json[displayedExoPs[epsilon]].pl_hostname} is ${Json[displayedExoPs[epsilon]].st_dist} light years away from our Sun.`
+        )
+    }
+    $(divDesired).show();
+}
+
+function printOrbit(Json, displayedExoPs, beta) {  
+    let zeta = beta.charAt(1);
+    let div = "#" + zeta + "Orbit";
+    console.log(Json[displayedExoPs[zeta]].pl_orbper);
+    $(div).empty();
+    if (Json[displayedExoPs[zeta]].pl_orbper === null) {
+        $(div).append(
+            `NASA currently does not have information on how long it takes for ${Json[displayedExoPs[zeta]].pl_name} to orbit its star.`
+        )
+    }
+    else {
+        let num = Json[displayedExoPs[zeta]].pl_orbper
+        let timeArr = [];
+        timeArr.push(Math.floor(num/365));
+        let remainder = num % 365;
+        timeArr.push(Math.floor(remainder/30));
+        timeArr.push(Math.floor(remainder % 30));
+        console.log(timeArr);
+        let time = timeArr[0] + " years, " + timeArr[1] + ' months, and ' + timeArr[2] + ' days' 
+        console.log(time);
+        
+        $(div).append(
+            `It takes ${Json[displayedExoPs[zeta]].pl_name} ${time} to orbit its host Star.`
+        )
+    }
+    $(div).show();
+}
+
+
+
 function printCompareRade(Json, n, displayedExoPs) {
     let desiredDiv = "#" + n + "SizeCompareBox";
+    $(desiredDiv).empty();
     console.log(Json[displayedExoPs[n]].pl_rade);
     if (Json[displayedExoPs[n]].pl_rade === null) {
         $(desiredDiv).append(
-            `<p>Sorry Nasa's data packet does not have a confirmed radius size for this Exoplanet.</p>`
+            `<p>Sorry NASA's data packet does not have a confirmed radius size for this Exoplanet.</p>`
     )}
     else {  
     $(desiredDiv).append(
@@ -92,7 +138,7 @@ function printCompareRade(Json, n, displayedExoPs) {
                     </div>
                 </div>
             </div>
-            <div id='exoPlanet'>
+            <div id='${n}exoPlanet'>
                 <div id="circleExoP">
                     <div id="circleTextExoPlanet">
                         <div id="textExoP">
@@ -111,8 +157,9 @@ function printCompareRade(Json, n, displayedExoPs) {
 function earthRadiusComparer(Json, n, displayedExoPs) {
     let adjWidth = (Json[displayedExoPs[n]].pl_rade * 200) + 'px';
     let adjHeight = (Json[displayedExoPs[n]].pl_rade * 200) + 'px';
-    document.getElementById("exoPlanet").style.height=adjHeight;
-    document.getElementById("exoPlanet").style.width=adjWidth;
+    let circleId = n + "exoPlanet";
+    document.getElementById(circleId).style.height=adjHeight;
+    document.getElementById(circleId).style.width=adjWidth;
 }
 
 function fetchRandomExoPlanet() {
@@ -126,7 +173,7 @@ function fetchRandomExoPlanet() {
     const url = baseURL + "?" + queryString;
   
     console.log(url);
-  
+    
     fetch(url)
       .then(response => {
         if (response.ok) {
@@ -142,7 +189,6 @@ function fetchRandomExoPlanet() {
 }
 
 function generateRandomPlanetsArr(responseJson) {
-    
     let desiredNum = $('#js-numberOfPlanets').val();
     let randomPlanets;
     let displayedExoPs = [];
@@ -167,23 +213,23 @@ function generateRandomPlanetsArr(responseJson) {
     printRandomExoPlanets(displayedExoPs, Json);
 }
 
-function listenToStarInfo() {
+function listenToStarInfo(Json, displayedExoPs) {
     $(document).on("click", ".starInfo", function(event) {
         event.preventDefault();
         let gamma = $(this).prop('id');
         let delta = "." + gamma;
         console.log(delta);
-        $(delta).show();
+        printStarInfo(Json, displayedExoPs, delta);   
     })
 }
 
-function listenToOrbit() {
+function listenToOrbit(Json, displayedExoPs) {
     $(document).on('click', '.orbit', function(event) {
         event.preventDefault();
         let alpha = $(this).prop('id');
         let beta = "." + alpha;
         console.log(beta);
-        $(beta).show(); 
+        printOrbit(Json, displayedExoPs, beta) 
     })
 }
 
@@ -199,9 +245,10 @@ function listenToCompare(Json, displayedExoPs) {
 
 function watchForm() {
     $('form').submit(event => {
-      event.preventDefault();
-      $('#results').show();
-      fetchRandomExoPlanet();
+        event.preventDefault();
+        
+        $('#results').show();
+        fetchRandomExoPlanet();
     });
 }
 
